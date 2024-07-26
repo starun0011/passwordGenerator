ec44d20835d3f02d453924f0b77cf676136bef69
Tarun Soni <starun6392@gamil.com>
2024-07-26 22:50:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { useCallback, useState, useEffect , useRef } from "react";
+// import "./App.css";
+
+function App() {
+  const [length, setlength] = useState(8);
+  const [numAllow, setNumAllow] = useState(false);
+  const [symAllow, setSymAllow] = useState(false);
+  const [password, setPassword] = useState("");
+
+  let passwordRef = useRef(null);
+
+  const copyToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    // passwordRef.current.setSelectionRange(0, 3); // for range selection
+    // window.navigator.clipboard.writeText(password.substring(0,4)); //for range selection
+    passwordRef.current.setSelectionRange(0, length); 
+    window.navigator.clipboard.writeText(password);
+
+  },[password])
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numAllow) str += "1234567890";
+    if (symAllow) str += "#@$%^&*(){}[]<>.,:;'!~`|";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numAllow, symAllow, setPassword]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numAllow, symAllow, passwordGenerator]);
+
+  return (
+    <>
+      <div className="text-white border  rounded-[12px] container mx-auto  mt-[10vh] max-w-xl text-center text-3xl py-4 bg-gray-600">
+        Password Generator
+        <div className=" flex gap-6 md:pl-9 pl-6  mt-2">
+          <input
+            type="text"
+            value={password}
+            placeholder="password"
+            readOnly
+            ref={passwordRef}
+            className="text-lg  text-[16px] px-2 w-96 rounded text-black
+      "
+          />
+          <button 
+          className="border rounded bg-green-800 text-base p-1"
+          onClick={copyToClipboard}
+          >
+            Copy
+          </button>
+        </div>
+        <div className="flex mt-2  text-base gap-2 md:pl-9 pl-6">
+          <input
+            type="range"
+            min={6}
+            max={20}
+            value={length}
+            onChange={(e) => {
+              setlength(e.target.value);
+            }}
+            className="cursor-pointer"
+          />
+          <label>length: {length}</label>
+          <input
+            type="checkbox"
+            defaultChecked={numAllow}
+            onChange={() => {
+              setNumAllow((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberAllow">Number</label>
+          <input
+            type="checkbox"
+            defaultChecked={symAllow}
+            onChange={() => {
+              setSymAllow((prev) => !prev);
+            }}
+          />
+          <label htmlFor="symbolAllow">Symbols</label>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
